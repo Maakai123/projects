db6f2c501e413d9cda6db123c934ef9af56c3a87
maakai princewill <maakaiprincewill@gmail.com>
2023-10-06 17:47:48 +0100

    update

--- Javascript-file/DOM/dom.js
+++ Javascript-file/DOM/dom.js
@@ -1,5 +1,132 @@
 'use strict';
 
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1')
+
+
+const openModal = function (e) {
+  e.preventDefault()
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+/*for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);*/
+
+  //use for Each Method 
+  btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal))
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+
+
+
+//BUTTON SCROLLING 
+
+
+btnScrollTo.addEventListener('click', function(e){
+  //co-ordinate
+  const s1cords = section1.getBoundingClientRect();
+  console.log(s1cords)
+  
+  //current scroll
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset)
+
+  //SCROLLING
+   //old way
+  /*window.scrollTo(
+    s1cords.left + window.pageXOffset,
+    s1cords.top + window.pageYOffset
+  )
+ */
+//2
+/*
+  window.scrollTo({ 
+  left: s1cords.left + window.pageXOffset,
+   top: s1cords.top + window.pageYOffset,
+   behavior: 'smooth',
+})
+*/
+
+//Modern way 
+
+//take the section we want to scroll to
+
+section1.scrollIntoView({behavior:  'smooth'})
+})
+
+/******************* *****************
+EVENT DELIGATION
+*************************************/
+// Page Navigations 
+
+
+
+/*
+//1) method but it has limitations 
+
+document.querySelectorAll('.nav__link').forEach(function(el){
+  el.addEventListener('click', function(e){
+    e.preventDefault() //stop unneccesary scrolling;
+    const id = this.getAttribute('href')// this =current Target
+    console.log(id) //return each id #section--1,#section--2,#section--2 when click on each of them
+  
+
+//add the scroll to each id's
+document.querySelector(id).scrollIntoView({
+  behavior:'smooth'
+})
+
+  })
+})
+*/
+
+//2)Method 2 Event deligations
+
+//a Add event listener to common parent element
+//b Determine what element originated the event
+
+document.querySelector('.nav__link').addEventListener //Target each links
+('click', function(e) {
+  e.preventDefault() //stop unneccesary scrolling;
+
+  //Matching strategy: will not allow us click on what is not link
+
+  if(e.target.classList.contains('nav__link')) { // if e.target has the nav__link
+  const id = e.target.getAttribute('href');//return each id #section--1,#section--2,#section--2 when click on each of them
+  
+  document.querySelector(id).scrollIntoView({
+    behavior: 'smooth'
+  })
+
+  }
+})
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////
+
 //How to Select Elements
 
 console.log(document.documentElement)
@@ -70,13 +197,18 @@ document
 
 
 
+
+
+
+
+
 /******************* *****************
 STYLES, ATTRIBUTES AND CLASSES
 *************************************/
 
 //STYLES 
 //This styles are inline styles, the once we created ourselves
-
+/*
 message.style.backgroundColor = '#37383d';
 message.style.width = '120%';
 
@@ -91,13 +223,14 @@ console.log(message.style.backgroundColor)// This will work because
 
 //lets use or access all the real styles 
 /* parseFloat will convert decimals strings  to numbers*/
-message.style.height = Number.parseFloat(getComputedStyle(message).height, 10)+ 30 + 'px'
+
+/*message.style.height = Number.parseFloat(getComputedStyle(message).height, 10)+ 30 + 'px'
 
 
 /* custom variables  */
 //documentElemt = root where the variables are defined 
 //change colors on the pages
-document.documentElement.style.setProperty('--color-primary', 'orangered')
+/*document.documentElement.style.setProperty('--color-primary', 'orangered')
 
 
 //ATTRIBUTES 
@@ -119,3 +252,124 @@ logo.setAttribute('company', 'Bankist')
 
 //10:18
 
+//Data attributes 
+//They start with the word data, use to store data in the user interface
+console.log(logo.dataset.versionNumber)
+
+//clases
+logo.classList.add('c','j')
+logo.classList.remove('c','j')
+logo.classList.toggle('c')
+logo.classList.contains('c')
+
+
+//dont use
+logo.className
+
+//Smooth scrolling 
+*/
+
+//scrolling effect
+
+/*
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1')
+
+btnScrollTo.addEventListener('click', function(e){
+  //co-ordinate
+  const s1cords = section1.getBoundingClientRect();
+  console.log(s1cords)
+  
+  //current scroll
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset)
+
+  //SCROLLING
+   //old way
+  /*window.scrollTo(
+    s1cords.left + window.pageXOffset,
+    s1cords.top + window.pageYOffset
+  )
+ */
+//2
+/*
+  window.scrollTo({ 
+  left: s1cords.left + window.pageXOffset,
+   top: s1cords.top + window.pageYOffset,
+   behavior: 'smooth',
+})
+
+
+
+//Modern way 
+
+//take the section we want to scroll to
+
+section1.scrollIntoView({behavior:  'smooth'})
+})
+
+ 
+//EVENTS 
+
+const h1 = document.querySelector('h1')
+/*
+h1.addEventListener('mouseenter', function(e){
+  alert('addEventListener: you are reading the heading : D')
+})
+
+const alertH1 = function(e) {
+  alert('addEventListener: you are reading the heading : D')
+}
+
+h1.addEventListener('mouseenter', alertH1)
+//remove event after 3s
+
+setTimeout(()=> h1.removeEventListener('mouseenter', alertH1), 3000)
+
+//WE CAN USE ONMOUSEENTER
+//old school, onclick;
+/*h1.onmouseenter = function (e) {
+  alert('addEventListener: you are reading the heading : D')
+}
+
+
+//GENERATE RANDOM COLORS
+
+//rgb(255,255,255)
+
+//generate colors between 0 and 255
+
+
+const randomInt = (min, max) => 
+Math.floor(Math.random() *(max - min + 1) + min);
+console.log(randomInt)
+const randomColor = () => 
+`rgba(${randomInt(0,255)},${randomInt(0,255)},${randomInt(0,255)})`
+
+console.log( randomColor())
+
+const navLink = document.querySelector('.nav__link')
+
+navLink.addEventListener('click',function(e){
+ 
+  this.style.backgroundColor = randomColor()
+  console.log('LINK', e.target)
+
+  //STOP PROPAGATION
+
+  //e.stopPropagation()
+})
+
+document.querySelector('.nav__links').addEventListener('click', function(e){
+  this.style.backgroundColor = randomColor()
+  console.log('CONTAINER', e.target)
+}) 
+
+  
+
+  document.querySelector('.nav').addEventListener('click', function(e){
+    this.style.backgroundColor = randomColor()
+    console.log('NAV', e.target)
+},)
+  */
+
+//Dom Traversing, 39: 28;
